9353092324d0ba9a2da01a5ce1a5ddfd5df1761f
hanosong <838519800@qq.com>
2022-10-29 15:16:21 +0800

    feat：分页器与页码组件算法

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,8 +1,30 @@
 import React, { memo } from "react";
+import Pagination from "@mui/material/Pagination";
 import { PaginationWrapper } from "./style";
+import { useSelector } from "react-redux";
 
 const EntirePagination = memo(() => {
-  return <PaginationWrapper>EntirePagination</PaginationWrapper>;
+  const { totalCount, currentPage, roomList } = useSelector((state) => ({
+    totalCount: state.entire.totalCount,
+    currentPage: state.entire.currentPage,
+    roomList: state.entire.roomList,
+  }));
+  const totalPage = Math.ceil(totalCount / 20); // 超出的数据必须多一页，不能向下取整
+  const startCount = currentPage * 20 + 1;
+  const endCount = (currentPage + 1) * 20;
+  // 如果某项数据没值的时候，不想展示，但是它又是0 => 转为布尔！！
+  return (
+    <PaginationWrapper>
+      {!!roomList.length && (
+        <div className="info">
+          <Pagination count={totalPage} />
+          <div className="desc">
+            第{startCount} - {endCount}个房源, 共超过{totalCount}个
+          </div>
+        </div>
+      )}
+    </PaginationWrapper>
+  );
 });
 
 export default EntirePagination;
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,5 +1,30 @@
 import styled from "styled-components";
 
-
+// MuiPaginationItem-icon 调整svg箭头的大小
 export const PaginationWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+
+    .info{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .Mui-selected.MuiPaginationItem-page{
+            background-color: #222;
+            color: #fff;
+        }
+        .MuiPaginationItem-page{
+            margin: 0 8px;
+            &:hover {
+                text-decoration: underline
+            }
+        }
+        .MuiPaginationItem-icon{
+            font-size: 25px;
+        }
+        .desc{
+            margin-top: 15px;
+            color: #222;
+        }
+    }
 `
