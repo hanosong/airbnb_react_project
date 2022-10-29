0706e852e2cb040e19749ff9ae70163147b1be41
hanosong <838519800@qq.com>
2022-10-29 21:05:15 +0800

    upd: 轮播图功能

---
+++ src/base-ui/indicator/index.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import React, { memo } from "react";
+import { IndicatorWrapper } from "./style";
+
+const Indicator = memo((props) => {
+  return <IndicatorWrapper>Indicator</IndicatorWrapper>;
+});
+
+Indicator.propTypes = {};
+
+export default Indicator;
--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,10 +1,20 @@
 import PropTypes from "prop-types";
-import React, { memo } from "react";
+import React, { memo, useRef } from "react";
+import { Carousel } from "antd";
 import { ItemWrapper } from "./style";
 import { Rating } from "@mui/material";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const RoomItem = memo((props) => {
   const { itemData, itemWidth = "25%" } = props;
+  const sliderRef = useRef();
+  /**  事件处理的逻辑*/
+  function controlClickHandle(isRignt = true) {
+    // 上一个面板， 下一个面板
+    isRignt ? sliderRef.current.next() : sliderRef.current.prev();
+  }
+
   return (
     // verifyColor 从服务器动态获取颜色
     <ItemWrapper
@@ -12,8 +22,34 @@ const RoomItem = memo((props) => {
       itemWidth={itemWidth}
     >
       <div className="inner">
-        <div className="cover">
+        {/* <div className="cover">
           <img src={itemData.picture_url} alt={1} />
+        </div> */}
+        {/* 改成轮播图 */}
+        <div className="slider">
+          <div className="control">
+            <div
+              className="btn left"
+              onClick={(e) => controlClickHandle(false)}
+            >
+              <IconArrowLeft width="30" height="30px" />
+            </div>
+            <div
+              className="btn right"
+              onClick={(e) => controlClickHandle(true)}
+            >
+              <IconArrowRight width="30" height="30px" />
+            </div>
+          </div>
+          <Carousel dots={false} ref={sliderRef}>
+            {itemData?.picture_urls?.map((item) => {
+              return (
+                <div className="cover" key={item}>
+                  <img src={item} alt="" />
+                </div>
+              );
+            })}
+          </Carousel>
         </div>
         <div className="desc">{itemData.verify_info.messages.join("·")}</div>
         <div className="name">{itemData.name}</div>
