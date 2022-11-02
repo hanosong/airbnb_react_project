1a452c973b51831e261ac37514729c8b837c4661
hanosong <838519800@qq.com>
2022-11-02 20:03:15 +0800

    upd：detail图片展示；fixed：head-right的样式问题

--- src/views/detail/c-cpns/detail-pictures/index.jsx
+++ src/views/detail/c-cpns/detail-pictures/index.jsx
@@ -6,7 +6,31 @@ const DetailPictures = memo(() => {
   const { detailInfo } = useSelector((state) => ({
     detailInfo: state.detail.detailInfo,
   }));
-  return <PicturesWrapper>DetailPictures</PicturesWrapper>;
+  return (
+    <PicturesWrapper>
+      <div className="pictures">
+        <div className="left">
+          <div className="item">
+            <img src={detailInfo.picture_urls?.[0]} alt="" />
+            <div className="cover"></div>
+          </div>
+        </div>
+        <div className="right">
+          {
+            // 只拿四条数据
+            detailInfo.picture_urls?.slice(1, 5).map((item) => {
+              return (
+                <div className="item" key={item}>
+                  <img src={item} alt="" />
+                  <div className="cover"></div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    </PicturesWrapper>
+  );
 });
 
 export default DetailPictures;
