0bf8e3aa1e30bb21557a3e8ee544be0732e6d1dd
hanosong <838519800@qq.com>
2023-12-13 22:37:34 +0800

    更新

--- craco.config.js
+++ craco.config.js
@@ -20,6 +20,8 @@ module.exports = {
   ],
   // 修改webpack
   webpack: {
+    mode: "development",
+    devtool: "source-map",
     // 配置别名;value必须是绝对路径
     // resolve => 拼接路径 ； __dirname:当前文件所在的路径
     alias: {
@@ -30,19 +32,19 @@ module.exports = {
       // '@mui/styled-engine': '@mui/styled-engine-sc'
     },
     plugins: [
-      new BundleAnalyzerPlugin({
-        /**
-         * server | static | disabled
-         * server： 分析器将启用HTTP服务器来显示打包报告
-         * static： 会生成带有报告的单个HTML文件
-         * disabled： 可以使用该插件来讲generateStatsFile 设置为 true 来生成webpack stats JSON文件
-         */
-        analyzerMode: "server",
-        analyzerHost: "127.0.0.1", // 在服务器模式下使用的主机启用HTTP服务器
-        analyzerPort: 8888, // 在服务器模式下使用的端口启动服务器
-        openAnalyzer: true, // 构建完关上浏览器，自动打开报告
-        reportFilename: path.resolve(__dirname, `analyzer/index.html`), // 输出目录
-      }),
+      // new BundleAnalyzerPlugin({
+      //   /**
+      //    * server | static | disabled
+      //    * server： 分析器将启用HTTP服务器来显示打包报告
+      //    * static： 会生成带有报告的单个HTML文件
+      //    * disabled： 可以使用该插件来讲generateStatsFile 设置为 true 来生成webpack stats JSON文件
+      //    */
+      //   analyzerMode: "server",
+      //   analyzerHost: "127.0.0.1", // 在服务器模式下使用的主机启用HTTP服务器
+      //   analyzerPort: 8888, // 在服务器模式下使用的端口启动服务器
+      //   openAnalyzer: true, // 构建完关上浏览器，自动打开报告
+      //   reportFilename: path.resolve(__dirname, `analyzer/index.html`), // 输出目录
+      // }),
     ],
   },
 };
